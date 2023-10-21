2a69243cd5971a691c7de5e5fd656de7de397586
rm <rafangel26@gmail.com>
2023-10-21 21:20:36 +0200

    task 2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -22,10 +22,11 @@ public class Calculator {
             System.out.println("2. odejmowanie");
             System.out.println("3. mnożenie");
             System.out.println("4. dzielenie");
-            System.out.println("5. ZAKOŃCZ");
+            System.out.println("5. Łączenie wyrazów");
+            System.out.println("6. ZAKOŃCZ");
             number = scan.nextByte();
 
-            while (number < 1 || number > 5) {
+            while (number < 1 || number > 6) {
                 System.out.println("Podałeś zły numer!");
                 number = scan.nextByte();
             }
@@ -48,6 +49,10 @@ public class Calculator {
                     divOperation();
                     break;
                 case 5:
+                    System.out.println("Wybrałeś łączenie wyrazów");
+                    concatOperation();
+                    break;
+                case 6:
                     calculate = false;
                     System.out.println("Do zobaczenia!");
             }
@@ -59,8 +64,10 @@ public class Calculator {
         Scanner scan1 = new Scanner(in);
         scan1.useLocale(Locale.US); //wprowadzanie liczb z kropką jako separatorem
         System.out.println("Podaj pierwszą liczbę:");
+        System.out.print(">>");
         a = scan1.nextDouble();
         System.out.println("Podaj drugą liczbę:");
+        System.out.print(">>");
         b = scan1.nextDouble();
         System.out.println(a + "+" + b + "=" + roundTo4DecimalPlace(a + b));
         System.out.println();
@@ -71,8 +78,10 @@ public class Calculator {
         Scanner scan1 = new Scanner(in);
         scan1.useLocale(Locale.US); //wprowadzanie liczb z kropką jako separatorem
         System.out.println("Podaj pierwszą liczbę:");
+        System.out.print(">>");
         a = scan1.nextDouble();
         System.out.println("Podaj drugą liczbę:");
+        System.out.print(">>");
         b = scan1.nextDouble();
         System.out.println(a + "-" + b + "=" + roundTo4DecimalPlace(a - b));
         System.out.println();
@@ -83,8 +92,10 @@ public class Calculator {
         Scanner scan1 = new Scanner(in);
         scan1.useLocale(Locale.US); //wprowadzanie liczb z kropką jako separatorem
         System.out.println("Podaj pierwszą liczbę:");
+        System.out.print(">>");
         a = scan1.nextDouble();
         System.out.println("Podaj drugą liczbę:");
+        System.out.print(">>");
         b = scan1.nextDouble();
         System.out.println(a + "*" + b + "=" + roundTo4DecimalPlace(a * b));
         System.out.println();
@@ -95,8 +106,10 @@ public class Calculator {
         Scanner scan1 = new Scanner(in);
         scan1.useLocale(Locale.US); //wprowadzanie liczb z kropką jako separatorem
         System.out.println("Podaj pierwszą liczbę:");
+        System.out.print(">>");
         a = scan1.nextDouble();
         System.out.println("Podaj drugą liczbę różną od zera:");
+        System.out.print(">>");
         b = scan1.nextDouble();
         while (b == 0.0) {
             System.out.println("Podaj liczbę różną od zera:");
@@ -108,5 +121,27 @@ public class Calculator {
 
     static double roundTo4DecimalPlace(double value) {
         return Math.round(value * 10000.0) / 10000.0;
+    }
+
+    void concatOperation() {
+        System.out.println("Podaj liczbę wyrazów do połącznia (max 20):");
+        Scanner scan2 = new Scanner(in);
+        String sentence = "";
+        String word = "";
+        byte count = scan2.nextByte();
+        while (count < 2 || count > 20) {
+            System.out.println("Podaj liczbę od 2 do 20:");
+            count = scan2.nextByte();
+        }
+        scan2.nextLine();
+        for (int i = 0; i < count; i++) {
+            System.out.println("Podaj " + (i + 1) + " słowo:");
+            word = scan2.nextLine();
+            if (word.contains(" "))  //jeśli podano więcej słów niż jedno, to wybierz pierwsze
+                word = word.substring(0, word.indexOf(" "));
+            sentence = sentence + word + " ";
+        }
+        sentence = sentence.trim();
+        System.out.println(sentence);
     }
 }
